57d2b88f92115a400f86893f88171d399b3bef8b
Luis Felipe Cruz <lfscruz@outlook.com>
2025-06-30 16:28:02 -0300

    initial grid setup

--- gol.c
+++ gol.c
@@ -2,24 +2,41 @@
 #include <stdlib.h>
 #include <time.h> 
 
-#define HEIGHT 20
-#define WIDTH 20
+#define HEIGHT 30
+#define WIDTH 30
 
-int main() {
-	srand(time(NULL));
-	int grid[HEIGHT][WIDTH];
-	
-	for (int i = 0; i < HEIGHT; i++) {
+void print_grid(char grid[HEIGHT][WIDTH]) {
+	for (int i = 0; i < HEIGHT; i++){
 		for (int j = 0; j < WIDTH; j++) {
-			int choice = rand() % 100;
-			if (choice < 10){
-				printf("# ");
-			} else {
-				printf("- ");
-			}
+			printf(" %c", grid[i][j]);
 		}
 		printf("\n");
 	}
+}
+
+void set_initial_grid(char grid[HEIGHT][WIDTH]) {
+	for (int i = 0; i < HEIGHT; i++) {
+		for (int j = 0; j < WIDTH; j++) {
+			int choice = rand() % 100;
+			if (choice < 5){
+				grid[i][j] = '#';
+			} else {
+				grid[i][j] = '-';
+			}
+		}
+	}
+
+}
+
+int main() {
+	srand(time(NULL));
+	char grid[HEIGHT][WIDTH];
+
+	if (grid[0][0] != '#' || grid[0][0] != '-'){
+		set_initial_grid(grid);
+	}
+
+	print_grid(grid);
 
 	return 0;
 
